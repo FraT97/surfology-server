41beaf1b1bb336498682b457faec1ce6c14188f6
Francesco Torchia <francescotorchia10@gmail.com>
2025-04-08 21:27:48 +0200

    connected

--- app.js
+++ app.js
@@ -10,9 +10,8 @@ require("./db");
 const express = require("express");
 const mongoose = require('mongoose');
 const app = express();
-const cors = require('cors');
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+
 
 const User = require('./models/User.model');
 const Product = require('./models/product.model');
